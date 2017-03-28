03e5bca4cb45633ada4f78dba33af8e1ab81e770
Hidetoshi Imai <hidepin@gmail.com>
2017-03-29 07:41:13 +0900

    Add setopt.

--- main.go
+++ main.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"reflect"
+	"syscall"
 )
+
+func bindToIf(conn net.Conn, interfaceName string) {
+	ptrVal := reflect.ValueOf(conn)
+	val := reflect.Indirect(ptrVal)
+	//next line will get you the net.netFD
+	fdmember := val.FieldByName("fd")
+	val1 := reflect.Indirect(fdmember)
+	netFdPtr := val1.FieldByName("sysfd")
+	fd := int(netFdPtr.Int())
+	//fd now has the actual fd for the socket
+	err := syscall.SetsockoptString(fd, syscall.SOL_SOCKET,
+		syscall.SO_BINDTODEVICE, interfaceName)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 
@@ -29,11 +48,23 @@ func main() {
 			}
 		}
 	} else {
-		conn, err := net.Dial("udp4", "localhost:8888")
+		local, err := net.ResolveUDPAddr("udp", ":5060")
+		if err != nil {
+			panic(err)
+		}
+
+		remote, err := net.ResolveUDPAddr("udp", "localhost:8888")
+		if err != nil {
+			panic(err)
+		}
+
+		conn, err := net.DialUDP("udp4", local, remote)
 		if err != nil {
 			panic(err)
 		}
 		defer conn.Close()
+		bindToIf(conn, "bond0")
+
 		fmt.Println("Sending to server")
 		_, err = conn.Write([]byte("Hello from Client"))
 		if err != nil {
